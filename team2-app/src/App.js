7518ebe9bb90e394024c726478313ce872c309c3
Krzysztof Koczorski <krzysztof.koczorski@gmail.com>
2020-02-23 13:27:15 +0100

    basic footer added

--- team2-app/src/App.js
+++ team2-app/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import Footer from "./Components/Footer";
 // import ResponsiveDrawer from "./Components/Nav";
 
 function App() {
   return (
     <div>
       <p>Apps</p>
+
+      <Footer />
     </div>
   );
 }
---
+++ team2-app/src/Components/Footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import FolderIcon from "@material-ui/icons/Folder";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+
+const Footer = () => {
+  return (
+    <div style={{ position: "absolute", bottom: "0" }}>
+      <BottomNavigation value={"value"} onChange={() => {}} className={""}>
+        <BottomNavigationAction
+          label="Recents"
+          value="recents"
+          icon={<RestoreIcon />}
+        />
+        <BottomNavigationAction
+          label="Favorites"
+          value="favorites"
+          icon={<FavoriteIcon />}
+        />
+        <BottomNavigationAction
+          label="Nearby"
+          value="nearby"
+          icon={<LocationOnIcon />}
+        />
+        <BottomNavigationAction
+          label="Folder"
+          value="folder"
+          icon={<FolderIcon />}
+        />
+      </BottomNavigation>
+    </div>
+  );
+};
+
+export default Footer;
