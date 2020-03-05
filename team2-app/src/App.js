fe2054f3d6f961b10778efc36a2617ed42b28ffe
Kasia Kulik <kulik.katarzyna@gmail.com>
2020-03-05 16:53:25 +0100

    Simple nav added

--- team2-app/src/App.js
+++ team2-app/src/App.js
@@ -1,12 +1,43 @@
 import React from "react";
 import "./App.css";
 import Footer from "./Components/Footer";
+
+import SegmentExampleRaised from "./Components/AllPlans/AllPlans.js";
+import { BrowserRouter, NavLink, Switch, Route } from "react-router-dom";
+
+import CenteredGrid from "./Components/AllPlans/AllPlans";
+
 // import ResponsiveDrawer from "./Components/Nav";
 
 function App() {
   return (
     <div>
       <p>Apps</p>
+      <header>
+        <BrowserRouter>
+          <nav>
+            <ul>
+              <li>
+                <NavLink exact to="/">
+                  <span role="img">🏠</span> Move On
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/Components/AllPlans/AllPlans.js">
+                  All Plans
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="container">
+            <Switch>
+              {/* <Route path="/components/test/SignIn" component={SignIn} /> */}
+              <Route path="/Components/AllPlans" component={CenteredGrid} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </header>
 
       <Footer />
     </div>
---
+++ team2-app/src/Components/AllPlans/AllPlans.js
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./AllPlans.module.css";
+import { Segment } from "semantic-ui-react";
+
+const SegmentExampleRaised = () => (
+  <Segment className={style.segmentExampleRaised} raised>
+    Pellentesque habitant morbi tristique senectus.
+  </Segment>
+);
+export default SegmentExampleRaised;
