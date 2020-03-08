8b49cf0e35ebd0fc8140976e6ecc24dcf9f12569
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2020-03-08 18:00:09 +0100

    Added some styles myPlan

--- src/Components/MyPlan/SlimmerTraining.js
+++ src/Components/MyPlan/SlimmerTraining.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
+import StartNow from "../AllPlans/StartNow";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 import Slimmer from "../../Data/Slimmer";
 import "../../App.css";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: 400,
+    width: 300,
     margin: 10
   },
   margin: {
@@ -27,45 +25,36 @@ const SlimmerTraining = () => {
   const classes = useStyles();
 
   return (
-    <div className="training__container">
-      <h1 className="myPlan__title">MY PLAN - SLIMMER</h1>
-      <h1 className="myPlan__subtitle">
-        Check exercises and when you're ready click:{" "}
-        <Button
-          variant="contained"
-          size="large"
-          color="primary"
-          className={classes.margin}
-        >
-          <b>START!!!</b>
-        </Button>
-      </h1>
-      {Slimmer.map(item => (
-        <Card key={item.id} className={classes.root}>
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="h2"
-              className="trainingCard__header"
-            >
-              {item.name}
-            </Typography>
-          </CardContent>
-          <CardMedia
-            component="img"
-            alt={item.name}
-            image={item.image}
-            title={item.name}
-          />
+    <>
+      <h1 className="myPlan__title">MY PLAN - SLIMMER - DAY 1</h1>
 
-          <CardActions className="trainingCard__checkIcon">
-            Time: 30 seconds
-            <CheckBoxOutlineBlankIcon />
-          </CardActions>
-        </Card>
-      ))}
-    </div>
+      <div className="training__container">
+        {Slimmer.map(item => (
+          <Card key={item.id} className={classes.root}>
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="h2"
+                className="trainingCard__header"
+              >
+                {item.name}
+              </Typography>
+            </CardContent>
+            <img
+              alt={item.name}
+              src={item.image}
+              className="trainingCard__image"
+            />
+
+            <CardActions className="trainingCard__checkIcon">
+              <StartNow />
+              <span className="trainingCard__time">30s</span>
+            </CardActions>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 };
 
