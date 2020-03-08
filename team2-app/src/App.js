dacbf0317bd1da41cc35426b01d245bb74092630
Maciej Ratynski <mcratyn@gmail.com>
2020-03-08 10:39:19 +0100

    deleted react dom from files where not needed

--- team2-app/src/App.js
+++ team2-app/src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavContainer from "./Components/Navigation/NavContainer";
+import AllPlans from "./Components/AllPlans/AllPlans";
+import Calendar from "./Components/Calendar/Calendar";
+import MyPlan from "./Components/MyPlan/MyPlan";
+import MyProfile from "./Components/MyProfile/MyProfile";
+import WhyMoveOn from "./Components/WhyMoveOn/WhyMoveOn";
+
 // import ResponsiveDrawer from "./Components/Nav";
 
 function App() {
   return (
-    <div>
-      <p>Apps</p>
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavContainer />
+        <hr />
+        <Switch>
+          <Route exact path="/" component={WhyMoveOn} />
+          <Route exact path="/all-plans" component={AllPlans} />
+          <Route exact path="/my-plan" component={MyPlan} />
+          <Route exact path="/calendar" component={Calendar} />
+          <Route exact path="/my-profile" component={MyProfile} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ team2-app/src/Components/AllPlans/AllPlans.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllPlans = () => {
+  return <div>All Plans</div>;
+};
+
+export default AllPlans;
