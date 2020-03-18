a1205b4b1007a059d4ccac3912e69cc74f6de85c
Krzysztof Koczorski <krzysztof.koczorski@gmail.com>
2020-03-18 11:44:41 +0100

    simple calendar added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NavContainer from "./Components/Navigation/NavContainer";
 import AllPlans from "./Components/AllPlans/AllPlans";
-import Calendar from "./Components/Calendar/Calendar";
 import MyPlan from "./Components/MyPlan/SlimmerTraining";
 import MyProfile from "./Components/MyProfile/MyProfile";
 import WhyMoveOn from "./Components/WhyMoveOn/WhyMoveOn";
+import Schedule from "./Components/Calendar/Calendar";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/" component={WhyMoveOn} />
           <Route exact path="/all-plans" component={AllPlans} />
           <Route exact path="/my-plan" component={MyPlan} />
-          <Route exact path="/calendar" component={Calendar} />
+          <Route exact path="/calendar" component={Schedule} />
           <Route exact path="/my-profile" component={MyProfile} />
         </Switch>
       </div>
--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-const Calendar = () => {
-  return <p>Calendar</p>;
-};
+import {
+  Scheduler,
+  DayView,
+  Appointments
+} from "@devexpress/dx-react-scheduler-material-ui";
 
-export default Calendar;
+const Schedule = () => (
+  <Scheduler
+    data={[
+      {
+        startDate: "2018-10-31 10:00",
+        endDate: "2018-10-31 11:00",
+        title: "Meeting"
+      },
+      {
+        startDate: "2020-18-03 18:00",
+        endDate: "2020-18-03 19:30",
+        title: "Go to a gym"
+      }
+    ]}
+  >
+    <DayView />
+    <Appointments />
+  </Scheduler>
+);
+
+export default Schedule;
