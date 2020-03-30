1bc6573893650440bbf2f179de72cc29f1ba68d2
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2020-03-30 21:11:03 +0200

    FZ13T2-24 added myPlan basic structure and modified usersData

--- src/Components/MyPlan/MyPlan.js
+++ src/Components/MyPlan/MyPlan.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Slimmer from "../../Data/Slimmer";
 import MyTrainingCard from "./MyTrainingCard";
+import Typography from "@material-ui/core/Typography";
 import "../../App.css";
 
 const MyPlan = () => {
   return (
     <>
-      <h1 className="myPlan__title">MY PLAN - SLIMMER - DAY 1</h1>
-
+      {/* <h1 className="myPlan__title"></h1> */}
+      <Typography variant="h2" component="h2" className="myPlan__title">
+        MY PLAN - SLIMMER - DAY 1
+      </Typography>
       <div className="training__container">
         {Slimmer.map(card => (
           <MyTrainingCard key={card.id} card={card} />
