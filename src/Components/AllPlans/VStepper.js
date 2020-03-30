7ca6d7b86326d2a2564d60c85d1563a3bd700f90
Kasia Kulik <kulik.katarzyna@gmail.com>
2020-03-30 18:55:35 +0200

    VStepper.js changed

--- src/Components/AllPlans/VStepper.js
+++ src/Components/AllPlans/VStepper.js
@@ -29,25 +29,20 @@ function getSteps() {
   return ["GET READY", "BE PREPARED", "DO THE JOB"];
 }
 
-function getStepContent(step) {
-  switch (step) {
+function getStepContent(index, step_1, step_2, step_3) {
+  switch (index) {
     case 0:
-      return `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`;
+      return step_1;
     case 1:
-      return "An ad group contains one or more ads which target a shared set of keywords.";
+      return step_2;
     case 2:
-      return `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`;
+      return step_3;
     default:
       return "Unknown step";
   }
 }
 
-export default function VStepper() {
+export default function VStepper({ step_1, step_2, step_3 }) {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const steps = getSteps();
@@ -71,7 +66,9 @@ export default function VStepper() {
           <Step key={label}>
             <StepLabel>{label}</StepLabel>
             <StepContent>
-              <Typography>{getStepContent(index)}</Typography>
+              <Typography>
+                {getStepContent(index, step_1, step_2, step_3)}
+              </Typography>
               <div className={classes.actionsContainer}>
                 <div>
                   <Button
