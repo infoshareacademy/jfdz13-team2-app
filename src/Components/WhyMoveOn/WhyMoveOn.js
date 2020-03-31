6419a52a4cff1f25f988e8674f44753515508f3f
Kamil Kieliszek <kamil.kieliszek@gmail.com>
2020-03-31 17:07:27 +0200

    Chartbords added

--- src/Components/WhyMoveOn/WhyMoveOn.js
+++ src/Components/WhyMoveOn/WhyMoveOn.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Doughnut, Bar, Line, Radar } from "react-chartjs-2";
 
 const WhyMoveOn = () => {
-  return <div>Why MOVE ON</div>;
+  let data = {
+    datasets: [
+      {
+        data: [10, 20, 30, 50],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.8)",
+          "rgba(54, 162, 235, 0.8)",
+          "rgba(255, 206, 86, 0.8)",
+          "rgba(255, 159, 64, 0.8)"
+        ]
+      }
+    ],
+
+    labels: ["Red", "Yellow", "Blue", "Green"]
+  };
+  return (
+    <div>
+      <Doughnut data={data} />
+      <Bar data={data} />
+      <Bar data={data} />
+    </div>
+  );
 };
 
 export default WhyMoveOn;
