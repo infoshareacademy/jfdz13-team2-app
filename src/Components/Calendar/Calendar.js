e844c435b99c932eb1de136046ed13b988ad2dd0
Krzysztof Koczorski <krzysztof.koczorski@gmail.com>
2020-04-04 09:55:27 +0200

    some changes in calendar

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -479,6 +479,7 @@ class AppointmentFormContainerBasic extends React.PureComponent {
 
     return (
       <AppointmentForm.Overlay
+        syles={{}}
         visible={visible}
         target={target}
         fullSize
