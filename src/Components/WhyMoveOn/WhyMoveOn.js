097a2dc5b8e9420cc5ed13821e6914e0d14e59fa
Kamil Kieliszek <kamil.kieliszek@gmail.com>
2020-04-04 16:24:51 +0200

    some of responsive problems fixed

--- src/Components/WhyMoveOn/WhyMoveOn.js
+++ src/Components/WhyMoveOn/WhyMoveOn.js
@@ -1,27 +1,62 @@
 import React from "react";
 import { Doughnut, Bar } from "react-chartjs-2";
+import "./WhyMoveOn.css";
 
 const WhyMoveOn = () => {
   let data = {
     datasets: [
       {
-        data: [10, 20, 30, 50],
+        data: [18, 3, 10, 5, 9],
         backgroundColor: [
           "rgba(255, 99, 132, 0.8)",
           "rgba(54, 162, 235, 0.8)",
+          "rgba(255, 206, 86, 0.8)",
           "rgba(255, 206, 86, 0.8)",
           "rgba(255, 159, 64, 0.8)"
         ]
       }
     ],
 
-    labels: ["Red", "Yellow", "Blue", "Green"]
+    labels: ["Red", "Yellow", "Blue", "Green", "Brown"],
+    options: {
+      title: {
+        display: true,
+        text: "Custom Chart Title"
+      }
+    }
+  };
+
+  let usersNumber = {
+    datasets: [
+      {
+        data: [40, 60],
+        backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(255, 159, 64, 0.8)"]
+      }
+    ],
+
+    labels: ["Kobiety", "Mężczyźni"],
+    options: {
+      title: {
+        display: true,
+        text: "Custom Chart Title"
+      }
+    }
   };
   return (
     <div>
-      <Doughnut data={data} />
-      <Bar data={data} />
-      <Bar data={data} />
+      <div className={"doughnutChart"}>
+        <Doughnut data={usersNumber} />
+      </div>
+
+      <div className={"charts"}>
+        <div className={"barChart"}>
+          <Bar data={data} />
+        </div>
+
+        <div className={"barChart"}>
+          <Bar data={data} />
+        </div>
+      </div>
     </div>
   );
 };
