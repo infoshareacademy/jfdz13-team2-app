7a6af4faed06ba0b94db6d6d184b8c3c7cbfe957
Kasia Kulik <kulik.katarzyna@gmail.com>
2020-05-07 22:37:48 +0200

    User update, chosen plan does not work

--- src/Components/AllPlans/AllPlans.js
+++ src/Components/AllPlans/AllPlans.js
@@ -7,6 +7,7 @@ import Heading from "./../Heading";
 // import HarderPlan from "./Components/MyPlan/HarderPlan";
 import PlanCard from "./PlanCard";
 import "./../../App.css";
+import UserProvider from "./../Aplication/UserProvider";
 
 class AllPlans extends React.Component {
   state = {
@@ -18,6 +19,19 @@ class AllPlans extends React.Component {
       chosenPlan: title
     });
     console.log(title);
+    fetch(
+      "https://jfdz13-team2-app.firebaseio.com/UsersData/-M6kbvzVHS4Za9cEhNP_",
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          myTrainingPlan: this.state.chosenPlan
+        })
+      }
+    ).then(() => {
+      this.setState({
+        myTrainingPlan: this.state.chosenPlan
+      });
+    });
   };
 
   componentDidMount() {
@@ -29,35 +43,44 @@ class AllPlans extends React.Component {
     const { data } = this.state;
 
     return (
-      <>
-        <Heading content="CHOOSE YOUR PERSONAL TRAINING PLAN" />
-        <div className={"training__container"}>
-          {data.map(plans => {
-            console.log("plans: ", plans);
-            return (
-              <PlanCard
-                component={Link}
-                to="/Components/MyPlan/SlimmerPlan"
-                id={plans.id}
-                avatar={plans.avatar}
-                title={plans.title}
-                subheader={plans.subheader}
-                goal={plans.goal}
-                rating={plans.rating}
-                content={plans.content}
-                step_1={plans.step_1}
-                step_2={plans.step_2}
-                step_3={plans.step_3}
-                image={plans.image}
-                jump={plans.jump}
-                key={plans.id} // dlatego wlasnie mapuje po id)
-                addedPlan={this.addedPlan}
-              ></PlanCard>
-            );
-          })}
-        </div>
-      </>
+      <UserProvider>
+        {user => {
+          return user ? (
+            <>
+              <Heading content="CHOOSE YOUR PERSONAL TRAINING PLAN" />
+              <div className={"training__container"}>
+                {data.map(plans => {
+                  console.log("plans: ", plans);
+                  return (
+                    <PlanCard
+                      component={Link}
+                      to="/Components/MyPlan/SlimmerPlan"
+                      id={plans.id}
+                      avatar={plans.avatar}
+                      title={plans.title}
+                      subheader={plans.subheader}
+                      goal={plans.goal}
+                      rating={plans.rating}
+                      content={plans.content}
+                      step_1={plans.step_1}
+                      step_2={plans.step_2}
+                      step_3={plans.step_3}
+                      image={plans.image}
+                      jump={plans.jump}
+                      key={plans.id} // dlatego wlasnie mapuje po id)
+                      addedPlan={this.addedPlan}
+                    ></PlanCard>
+                  );
+                })}
+              </div>
+            </>
+          ) : (
+            <h2>Sign In to choose your training plan!</h2>
+          );
+        }}
+      </UserProvider>
     );
   }
 }
+
 export default AllPlans;
